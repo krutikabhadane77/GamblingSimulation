b711c71995ec86499c25b0b90fb540c0a8932fd0
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-26 00:52:22 +0530

    Added: UC2 Gambler make bet so either win or loose

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -3,12 +3,26 @@ import java.util.Random;
 public class GamblingSimulation {
     static int stake = 100;
     static int bet = 1;
+    public static void winLoss()
+    {
+        Random random=new Random();
+        int bet=random.nextInt(2);
+        if (bet==1) {
+            System.out.println("Gambler won the bet");
+            stake += bet;
+        }
+        else
+        {
+            stake -= bet;
+            System.out.println("Gambler loose the bet");
+        }
 
+    }
 
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation Problem");
         System.out.println("Gambler every day stake is " + stake + "and bet is " + bet);
-
+        winLoss();
     }
 }
 
