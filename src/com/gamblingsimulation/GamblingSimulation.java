d09b7830b73d8200ec5cd861ef7d063db4b86e07
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-26 01:04:47 +0530

    Added: UC3 Gambler if won or lost 50% stake

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class GamblingSimulation {
     static int stake = 100;
     static int bet = 1;
+    static int betNumber=0;
     public static void winLoss()
     {
         Random random=new Random();
@@ -19,10 +20,32 @@ public class GamblingSimulation {
 
     }
 
+    public static int resignDay()
+    {
+        while (stake != 50 && stake != 150)
+        {
+            betNumber = betNumber + 1;
+            System.out.println("Current Bet number is: " +betNumber);
+            Random random=new Random();
+            int random_value=random.nextInt(2);
+            if(random_value == 0) {
+                stake = stake - bet;
+                System.out.println("Bet outcome is: Loss");
+            }
+            else {
+                stake = stake + bet;
+                System.out.println("Bet outcome is: Win");
+                System.out.println("Stake value is" +stake);
+            }
+        }
+        return stake;
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation Problem");
         System.out.println("Gambler every day stake is " + stake + "and bet is " + bet);
         winLoss();
+        resignDay();
     }
 }
 
