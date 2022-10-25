5f905c7d0ed0706aeeea83e75073dd3ee246f52b
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-26 00:49:36 +0530

    Added: UC1 Start with stake 100 per day and bet is 1 every game

---
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,16 @@
+package com.gamblingsimulation;
+import java.util.Random;
+public class GamblingSimulation {
+    static int stake = 100;
+    static int bet = 1;
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to Gambling Simulation Problem");
+        System.out.println("Gambler every day stake is " + stake + "and bet is " + bet);
+
+    }
+}
+
+
+
